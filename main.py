235a4f21c7d887b0332244d98f205295202916c6
Monkey-D-Luc <133494882+Monkey-D-Luc@users.noreply.github.com>
2023-10-04 12:58:50 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import sqlite3
+
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem, QMessageBox,QMainWindow,QLabel,QTimeEdit
+from PyQt5.QtCore import QTime
+from PyQt5.uic import loadUi
+import sys
+
+from PyQt5 import QtCore
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        loadUi("UI.ui", self)
+        self.label = self.findChild(QLabel, "label")
+
+        self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
+        self.calendarDateChanged()
+        self.saveButton.clicked.connect(self.saveChanges)
+        self.addButton.clicked.connect(self.addNewTask)
+
+    def calendarDateChanged(self):
+        dateSelected = self.calendarWidget.selectedDate().toPyDate().strftime("%d-%m-%y")
+        self.label_4.setText(dateSelected)
+        self.updateTaskList(dateSelected)
+
+    def updateTaskList(self, date):
+        self.tasksListWidget.clear()
+
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+
+        query = "SELECT task, completed FROM tasks WHERE date = ?"
+        row = (date,)
+        results = cursor.execute(query, row).fetchall()
+        for result in results:
+            item = QListWidgetItem(str(result[0]))
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            if result[1] == "YES":
+                item.setCheckState(QtCore.Qt.Checked)
+            elif result[1] == "NO":
+                item.setCheckState(QtCore.Qt.Unchecked)
+            self.tasksListWidget.addItem(item)
+
+
+    def saveChanges(self):
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+        date = self.calendarWidget.selectedDate().toPyDate().strftime("%d-%m-%y")
+
+        for i in range(self.tasksListWidget.count()):
+            item = self.tasksListWidget.item(i)
+            task = item.text()
+            if item.checkState() == QtCore.Qt.Checked:
+                query = "UPDATE tasks SET completed = 'YES' WHERE task = ? AND date = ?"
+            else:
+                query = "UPDATE tasks SET completed = 'NO' WHERE task = ? AND date = ?"
+            row = (task, date,)
+            cursor.execute(query, row)
+        db.commit()
+
+        messageBox = QMessageBox()
+        messageBox.setText("Changes saved.")
+        messageBox.setStandardButtons(QMessageBox.Ok)
+        messageBox.exec()
+
+    def addNewTask(self):
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+        date = self.calendarWidget.selectedDate().toPyDate().strftime("%d-%m-%y")
+        newTask = str(self.taskLineEdit.text())
+        ti =str(self.timeIn.text())
+        to =str(self.timeOut.text())
+        query = "INSERT INTO tasks(task, completed, date, timein,timeout) VALUES (?,?,?,?,?)"
+        row = (newTask, "NO", date,ti,to)
+        cursor.execute(query, row)
+        db.commit()
+        self.updateTaskList(date)
+        self.taskLineEdit.clear()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
